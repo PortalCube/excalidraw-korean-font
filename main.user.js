aeea57ef28e98f6f15e1ca49aee22a1056502cb2
PortalCube <35104213+PortalCube@users.noreply.github.com>
2022-10-14 22:17:19 +0900

    add main.user.js

---
+++ main.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Excalidraw Korean Font (temp version)
+// @namespace    PortalCube
+// @version      temp-1.0
+// @description  Excalidraw에 한국어 맞춤 폰트를 적용합니다.
+// @author       PortalCube
+// @include      https://excalidraw.com*
+// @include      https://app.excalidraw.com*
+// ==/UserScript==
+
+(function () {
+    "use strict";
+})();
