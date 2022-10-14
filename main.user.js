620a4879491613628d0b7450d097ad943c986d93
PortalCube <35104213+PortalCube@users.noreply.github.com>
2022-10-14 22:26:04 +0900

    feat: implemented main feature

--- main.user.js
+++ main.user.js
@@ -10,4 +10,35 @@
 
 (function () {
     "use strict";
+
+    const virgilOverrides = "https://excalidraw-olive.vercel.app/sdmisaeng.woff2";
+    const helveticaOverrides =
+        "https://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff2";
+    const cascadiaOverrides =
+        "https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_three@1.0/D2Coding.woff";
+
+    const styleOverrides = `
+      @font-face {
+        font-family: "Virgil";
+        src: url('${virgilOverrides}');
+        font-display: swap;
+      }
+      
+      @font-face {
+        font-family: "Helvetica";
+        src: url('${helveticaOverrides}');
+        font-display: swap;
+      }
+      
+      @font-face {
+        font-family: "Cascadia";
+        src: url('${cascadiaOverrides}');
+        font-display: swap;
+      }
+    `;
+
+    const styleElement = document.createElement("style");
+    styleElement.innerHTML = styleOverrides;
+
+    document.querySelector("head").appendChild(styleElement);
 })();
