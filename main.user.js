396bfeb21394634aba4b4331ac7464d01e90ee9b
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-03-06 01:03:46 +0900

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,44 +1,92 @@
 // ==UserScript==
-// @name         Excalidraw Korean Font (temp version)
-// @namespace    PortalCube
-// @version      temp-1.0
-// @description  Excalidraw에 한국어 맞춤 폰트를 적용합니다.
-// @author       PortalCube
-// @include      https://excalidraw.com*
-// @include      https://app.excalidraw.com*
+// @name        Excalidraw Korean Font
+// @namespace   PortalCube
+// @include     *://excalidraw.com*
+// @include     *://app.excalidraw.com*
+// @grant       none
+// @run-at      document-end
+// @version     1.0
+// @author      PortalCube
+// @description Excalidraw의 폰트를 한국어에 맞춘 폰트로 교체합니다.
 // ==/UserScript==
 
 (function () {
-    "use strict";
+    function CreateLinkNode(href) {
+        const node = document.createElement("link");
+        node.rel = "preload";
+        node.href = href;
+        node.as = "font";
+        node.setAttribute("crossorigin", "anonymous");
+        return node;
+    }
 
-    const virgilOverrides = "https://excalidraw-olive.vercel.app/sdmisaeng.woff2";
-    const helveticaOverrides =
+    const virgilReplace = "https://excalidraw-olive.vercel.app/sdmisaeng.woff2";
+    const helveticaReplace =
         "https://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff2";
-    const cascadiaOverrides =
+    const cascadiaReplace =
         "https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_three@1.0/D2Coding.woff";
 
-    const styleOverrides = `
-      @font-face {
+    const stylesheet = `
+    @font-face {
         font-family: "Virgil";
-        src: url('${virgilOverrides}');
+        src: url('${virgilReplace}');
         font-display: swap;
-      }
-      
-      @font-face {
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    @font-face {
         font-family: "Helvetica";
-        src: url('${helveticaOverrides}');
+        src: url('${helveticaReplace}');
         font-display: swap;
-      }
-      
-      @font-face {
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    @font-face {
         font-family: "Cascadia";
-        src: url('${cascadiaOverrides}');
+        src: url('${cascadiaReplace}');
         font-display: swap;
-      }
+        font-weight: normal;
+        font-style: normal;
+    }
     `;
 
-    const styleElement = document.createElement("style");
-    styleElement.innerHTML = styleOverrides;
+    const styleNode = document.createElement("style");
+    styleNode.innerHTML = stylesheet;
 
-    document.querySelector("head").appendChild(styleElement);
+    const virgilPreloadNode = CreateLinkNode(virgilReplace);
+    const helveticaPreloadNode = CreateLinkNode(helveticaReplace);
+    const cascadiaPreloadNode = CreateLinkNode(cascadiaReplace);
+    const headNode = document.querySelector("head");
+
+    headNode.append(
+        styleNode,
+        virgilPreloadNode,
+        helveticaPreloadNode,
+        cascadiaPreloadNode
+    );
+
+    // Remove original font-face
+    if (location.host === "app.excalidraw.com") {
+        // Excalidraw App
+        const observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                const addedNodes = mutation.addedNodes;
+                if (
+                    addedNodes.length > 0 &&
+                    addedNodes[0].innerHTML.includes("@font-face") &&
+                    addedNodes[0].innerHTML.includes("Virgil")
+                ) {
+                    observer.disconnect();
+                    addedNodes[0].innerHTML = "";
+                }
+            });
+        });
+
+        observer.observe(headNode, { childList: true });
+    } else if (location.host === "excalidraw.com") {
+        // Excalidraw
+        document.querySelector('link[href="fonts.css"]').remove();
+    }
 })();
