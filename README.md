4df83713abe6fb9ac6a83654e07861f626173ccd
PortalCube <35104213+PortalCube@users.noreply.github.com>
2024-11-23 03:05:24 +0900

    feat: excalidraw 폰트 기능 개선 업데이트에 대응

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # Excalidraw 한글 폰트
-<img width="960" alt="chrome_6TtmWj8jX6" src="https://user-images.githubusercontent.com/35104213/234362842-c293fa48-42f7-46ef-9fa5-e2c91a217e12.png">
 
-Excalidraw의 폰트를 한국어 사용자들에게 적합한 폰트로 교체해주는 Userscript입니다.
+<img width="960" alt="img" src="https://github.com/user-attachments/assets/ec95c7da-ca7b-4a01-876d-278c497e0660">
 
-excalidraw.com은 물론, Excalidraw+에서도 사용할 수 있습니다.
+Excalidraw의 폰트를 더욱 이쁜 한글 폰트로 교체하는 Userscript입니다.
 
 ## 사용하는 폰트
-| 모양 | 폰트 |
-|---|---|
-| 손글씨 | SD미생체(수정본) |
-| 일반 | Noto Sans KR |
-| 코드 | D2Coding |
+
+| 원본                | 변경                 | URL                              |
+| ------------------- | -------------------- | -------------------------------- |
+| Excalifont (손글씨) | 오뮤 다예쁨체        | https://noonnu.cc/font_page/1136 |
+| Nurito (고딕)       | 프리텐다드           | https://noonnu.cc/font_page/694  |
+| Comic Shanns (코딩) | D2Coding             | https://noonnu.cc/font_page/92   |
+| Lilita One (제목)   | 여기어때 잘난체 고딕 | https://noonnu.cc/font_page/1254 |
+
+폰트들은 모두 한글 11,172자를 지원합니다.
+
+코드의 "CUSTOM_FONTS" 부분에서 간단하게 원하는 폰트로 변경할 수도 있습니다.
 
 ## 사용법
 
@@ -20,8 +25,6 @@ excalidraw.com은 물론, Excalidraw+에서도 사용할 수 있습니다.
 
 ## 주의사항
 
-원본 폰트와 한글 폰트의 글자 폭이 다른 관계로, 여러 사용자가 사용하는 화이트보드에서 몇몇 인원만 한글 폰트를 적용하여 작업하게 될 경우 한글 폰트를 이용하지 않는 사용자는 화이트보드의 텍스트들의 요소가 정상적으로 표시되지 않을 수 있습니다.
+-   원본 폰트와 한글 폰트의 글자 폭이 다른 관계로, 여러 사용자가 사용하는 화이트보드에서 몇몇 인원만 한글 폰트를 적용하여 작업하게 될 경우 한글 폰트를 이용하지 않는 사용자는 화이트보드의 텍스트들의 요소가 정상적으로 표시되지 않을 수 있습니다.
 
-마찬가지로 한글 폰트를 사용하다가 한글 폰트를 삭제하는 경우, 한글 폰트 상태에서 작업한 화이트보드들의 텍스트들이 정상적으로 표시되지 않을 수 있습니다.
-
-이 점 유의하여 사용하시기 바랍니다.
+-   한글 폰트를 사용하다가 한글 폰트를 삭제하는 경우, 한글 폰트 상태에서 작업한 화이트보드들의 텍스트들이 정상적으로 표시되지 않을 수 있습니다.
--- main.user.js
+++ main.user.js
@@ -4,89 +4,77 @@
 // @include     *://excalidraw.com*
 // @include     *://app.excalidraw.com*
 // @grant       none
-// @run-at      document-end
-// @version     1.0
+// @run-at      document-start
+// @version     1.1
 // @author      PortalCube
 // @description Excalidraw의 폰트를 한국어에 맞춘 폰트로 교체합니다.
 // ==/UserScript==
 
+// Excalidraw에서 사용하는 폰트 목록
+// https://github.com/excalidraw/excalidraw/blob/b2a6a87b10f31babd1ee1a69b02a3f3f24ba492f/packages/excalidraw/constants.ts#L131
+
+const CUSTOM_FONTS = [
+    {
+        name: "Virgil", // 구버전 손글씨
+        url: "https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-01@1.0/omyu_pretty.woff2"
+    },
+    {
+        name: "Helvetica", // 구버전 고딕
+        url: "https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff"
+    },
+    {
+        name: "Cascadia", // 구버전 고정폭(코딩)
+        url: "https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_three@1.0/D2Coding.woff"
+    },
+    {
+        name: "Excalifont", // 손글씨체
+        url: "https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-01@1.0/omyu_pretty.woff2"
+    },
+    {
+        name: "Nunito", // 고딕체
+        url: "https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff"
+    },
+    {
+        name: "Comic Shanns", // 코딩체
+        url: "https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_three@1.0/D2Coding.woff"
+    },
+    {
+        name: "Lilita One", // 두꺼운 디자인서체
+        url: "https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_231029@1.1/JalnanGothic.woff"
+    }
+];
+
 (function () {
-    function CreateLinkNode(href) {
-        const node = document.createElement("link");
-        node.rel = "preload";
-        node.href = href;
-        node.as = "font";
-        node.setAttribute("crossorigin", "anonymous");
-        return node;
-    }
+    // Excalidraw는 폰트를 불러올 때 통상적인 CSS 방식을 사용하지 않고, FontFace 객체를 사용하여 폰트를 동적으로 로드합니다.
+    // Source: https://github.com/excalidraw/excalidraw/blob/b2a6a87b10f31babd1ee1a69b02a3f3f24ba492f/packages/excalidraw/fonts/ExcalidrawFontFace.ts#L24-L29
 
-    const virgilReplace = "https://excalidraw-olive.vercel.app/sdmisaeng.woff2";
-    const helveticaReplace =
-        "https://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff2";
-    const cascadiaReplace =
-        "https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_three@1.0/D2Coding.woff";
+    // 이 유저스크립트는 FontFace 객체에 Proxy를 붙이고 생성자를 호출하는 로직을 가로채는 방식으로 동작합니다.
 
-    const stylesheet = `
-    @font-face {
-        font-family: "Virgil";
-        src: url('${virgilReplace}');
-        font-display: swap;
-        font-weight: normal;
-        font-style: normal;
-    }
+    // 프록시 핸들러 작성
+    const handler = {
+        construct(target, args) {
+            let [fontName, source, options, ..._] = args;
 
-    @font-face {
-        font-family: "Helvetica";
-        src: url('${helveticaReplace}');
-        font-display: swap;
-        font-weight: normal;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: "Cascadia";
-        src: url('${cascadiaReplace}');
-        font-display: swap;
-        font-weight: normal;
-        font-style: normal;
-    }
-    `;
-
-    const styleNode = document.createElement("style");
-    styleNode.innerHTML = stylesheet;
-
-    const virgilPreloadNode = CreateLinkNode(virgilReplace);
-    const helveticaPreloadNode = CreateLinkNode(helveticaReplace);
-    const cascadiaPreloadNode = CreateLinkNode(cascadiaReplace);
-    const headNode = document.querySelector("head");
-
-    headNode.append(
-        styleNode,
-        virgilPreloadNode,
-        helveticaPreloadNode,
-        cascadiaPreloadNode
-    );
-
-    // Remove original font-face
-    if (location.host === "app.excalidraw.com") {
-        // Excalidraw App
-        const observer = new MutationObserver(function (mutations) {
-            mutations.forEach(function (mutation) {
-                const addedNodes = mutation.addedNodes;
-                if (
-                    addedNodes.length > 0 &&
-                    addedNodes[0].innerHTML.includes("@font-face") &&
-                    addedNodes[0].innerHTML.includes("Virgil")
-                ) {
-                    observer.disconnect();
-                    addedNodes[0].innerHTML = "";
+            for (const item of CUSTOM_FONTS) {
+                if (item.name !== fontName) {
+                    continue;
                 }
-            });
-        });
 
-        observer.observe(headNode, { childList: true });
-    } else if (location.host === "excalidraw.com") {
-        // Excalidraw
-        document.querySelector('link[href="fonts.css"]').remove();
-    }
+                // 폰트 url 교체
+                source = `url(${item.url})`;
+
+                // 영어권 문자만 적용하지 않도록, unicodeRange 제거
+                if (options.hasOwnProperty("unicodeRange")) {
+                    delete options.unicodeRange;
+                }
+
+                break;
+            }
+
+            return new target(fontName, source, options, ..._);
+        }
+    };
+
+    // 프록시 붙이기
+    window.FontFace = new Proxy(FontFace, handler);
 })();
